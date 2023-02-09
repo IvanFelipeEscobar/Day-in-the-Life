f755bdfc19624c5e5b429adc50b2c6a6aafc1f0c
Lauren Bentley <laurendebentley@gmail.com>
2023-02-09 18:19:12 -0500

    added jquery cdn to html files, started front-end sign up code

---
+++ public/js/signup.js
@@ -0,0 +1,13 @@
+async function signupForm(e) {
+    e.preventDefault()
+
+    const name = ''
+    const username =''
+    const email = ''
+    const password = ''
+    const bio = ''
+    const location = ''
+
+    
+
+}
