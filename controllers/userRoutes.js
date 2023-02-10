ea2ca62807d5390880a29aa66c940bff155f83a4
Ivan Escobar <ivnescbr1989@gmail.com>
2023-02-10 15:31:16 -0500

    user routes +

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const sequelize = require('../config/connection')
 const { Entry, User, Comment } = require('../models')
-
+//  /user routed
 router.get(`/`, async (req, res) => {
     try {
         const userData = await Entry.findAll({
@@ -28,7 +28,37 @@ router.get(`/`, async (req, res) => {
         res.status(500).json(err)       
     }
 })
+// /user/post/:id
+router.get(`/post/:id`, async (req, res) => {
+    try {
+     const byIdData = Entry.findOne({
+         where: {
+             id: req.session.id
+         },
+         attributes: [`id`, `entry_title`, `entry_content`, `created_at`],
+         include: [{
+             model: Comment,
+             attributes: [`id`, `comment_content`, `post_id`, `user_id`],
+             include: {
+                 model: User,
+                 attributes: `name`
+             }
+         },
+         {
+             model: User,
+             attributes: `name`
+         }]
+     })
+     if(!byIdData){
+         res.status(404).json({message: `no entries found`})
+     }
+     const singleEntry = byIdData.map((entry) => entry.get({plain:true}))
+     res.render(`singlePost`, {singleEntry, loggedIn: req.session.loggedIn})
+    } catch (err) {
+     res.status(500).json(err)
+    }
+})
 
-router.get()
-
+// user/create
+router.get(`/create`, (req, res) => res.render(`new-entry`))
 module.exports =  router
