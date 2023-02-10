98a66ecda47470d112d446111efbe1b31bec3020
Lauren Bentley <laurendebentley@gmail.com>
2023-02-10 18:54:40 -0500

    frontend login fetch created, needs event listener

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+const { application } = require("express")
+const { post } = require("../../controllers/api/usersRoutes")
+
+async function login(e) {
+    const email = $('#email').value.trim()
+    const password = $('#password').value.trim()
+
+    if(username && password) {
+        const response = await fetch('api/user/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: {
+                'Content-Type': "application/json"
+            }
+        }
+    )}
+        if(response.ok) {
+           console.log('Logged in!')
+           document.location.replace('/') 
+        }
+}
