632dfba00b436a5b3aefaddeea142d94c5842d63
Lauren Bentley <laurendebentley@gmail.com>
2023-02-10 18:37:55 -0500

    inital commit of frontend js fetch for signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,13 +1,37 @@
+const { application } = require("express")
+
 async function signupForm(e) {
     e.preventDefault()
 
-    const name = ''
-    const username =''
-    const email = ''
-    const password = ''
-    const bio = ''
-    const location = ''
+    const name = $('#name').value.trim()
+    const username =$('#username').value.trim()
+    const email = $('#email').value.trim()
+    const password = $('#password').value.trim()
+    const bio = $('#bio').value.trim()
+    const location = $('#location').value.trim()
 
-    
+    if (name && username && email && password && bio && location) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify( {
+                username,
+                password,
+                name,
+                password,
+                bio,
+                location
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+       });
+       if(response.ok) {
+        console.log('new user creatd!')
+        document.location.replace('/')
+       }
+    }
+
+
+
 
 }
