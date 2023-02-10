0660f2559375a2620fa15f9cf82b37d864824a60
Ivan Escobar <ivnescbr1989@gmail.com>
2023-02-09 19:01:01 -0500

    view engine set up

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,9 @@
 const router = require('express').Router()
 const sequelize = require('../config/connection')
 
+router.get(`/`, (req, res) => res.render(`home`))
+
+router.get(`/signup`, (req, res)=> res.render(`signup`))
+
+router.get(`/login`)
 module.exports =  router
--- server.js
+++ server.js
@@ -3,11 +3,36 @@ const routes = require('./controllers');
 const sequelize = require(`./config/connection`)
 // import sequelize connection
 
+const SequelizeStore = require(`connect-session-sequelize`)(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers })
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {
+    expires: 22 * 60 * 1000 //session expires after 22 minutes
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
+
+app.engine(`handlebars`, hbs.engine);
+app.set(`view engine`, `handlebars`);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, `public`)));
+
 
 app.use(routes);
 
