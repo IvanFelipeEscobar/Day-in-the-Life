08a7a73683381287af20405b56be70e2fe94e513
Ivan Escobar <ivnescbr1989@gmail.com>
2023-02-10 16:36:11 -0500

    api/entries route & other typos

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const commentRoutes = require(`./commentRoutes`)
 const journalEntryRoutes = require(`./journalEntryRoutes`)
-const userRoutes = require(`./userRoutes`)
+const userRoutes = require(`./usersRoutes`)
 
 router.use(`/comments`, commentRoutes)
 router.use(`/entries`, journalEntryRoutes)
